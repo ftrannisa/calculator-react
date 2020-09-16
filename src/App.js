69cece5439f9abc57e8d4c4b421d1dd0f05782c7
Fitri Annisa <ftrannisaa@gmail.com>
2020-09-16 18:20:19 +0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import "./App.css";
+import ResultDisplay from "./component/ResultDisplay";
+import KeypadDisplay from "./component/KeypadDisplay";
+
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      result: "",
+    };
+  }
+  onClick = (button) => {
+    if (button === "=") {
+      this.calculate();
+    } else if (button === "C") {
+      this.reset();
+    } else if (button === "CE") {
+      this.backspace();
+    } else {
+      this.setState({
+        result: this.state.result + button,
+      });
+    }
+  };
+
+  calculate = () => {
+    let checkResult = "";
+    if (this.state.result.includes("--")) {
+      checkResult = this.state.result.replace("--", "+");
+    } else {
+      checkResult = this.state.result;
+    }
+
+    try {
+      this.setState({
+        result: (eval(checkResult) || "") + "",
+      });
+    } catch (e) {
+      this.setState({
+        result: "error",
+      });
+    }
+  };
+
+  reset = () => {
+    this.setState({
+      result: "",
+    });
+  };
+
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1),
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="calculator">
+          <h1>Calculator</h1>
+          <ResultDisplay result={this.state.result} />
+          <KeypadDisplay onClick={this.onClick} />
+        </div>
+      </div>
+    );
+  }
+}
